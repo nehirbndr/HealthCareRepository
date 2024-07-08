7af62aa6f37a7766ca336d9d3a6b5656b0ed64aa
Nehir <nhrbndr@gmail.com>
2024-07-08 10:46:01 +0300

    producer ve consumer projeleri (düzeltme)

---
+++ Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace HealthCareManagement.Consumer
+{
+    class Consumer
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                
+                //Kafka broker adresi ve portu belirtilir.
+                BootstrapServers = "localhost:9092",
+                
+                //Tüketici grubun kimliği belirtilir.
+                GroupId = "hastalar",
+                
+                //Tüketici grubunun kaydedilmiş bir offseti yoksa veya offset mevcut değilse tüketici en eski mesajdan başlar.
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            //Tüketici nesnesi oluşturulur ve yapılandırılır.
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                
+                //Tüketici, ilgili topic'e abone olur, bu sayede gelen mesajlar dinlenir.
+                consumer.Subscribe("healthcare-topic");
+
+                //Kafkadan sürekli olarak mesaj alınır.
+                while (true)
+                {
+                    //Tüketici bir mesajı alır.
+                    var consumeResult = consumer.Consume();
+
+                    //Tüketilen mesaj değeri konsola yazdırılır.
+                    Console.WriteLine($"Alınan mesaj: {consumeResult.Message.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace HealthCareManagement.Producer
+{
+    class Producer
+    {
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                
+                //Kafka broker adresi ve portu belirtilir.
+                BootstrapServers = "localhost:9092"
+            };
+
+            //Üretici nesnesi oluşturulur ve yapılandırılır.
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                //Kullanıcıdan sürekli olarak mesaj alınır ve Kafka'ya gönderilir.
+                while (true) 
+                {
+                    Console.Write("Mesajı girin (çıkmak için 'çıkış' yazın): ");
+                    var message = Console.ReadLine();
+                    
+                    //Kullanıcının 'exit' yazması durumunda program sonlandırılır.
+                    if (message == "çıkış")
+                    {
+                        break;
+                    }
+                    
+                    //Mesaj ilgili topic'e asenkron olarak gönderilir ve sonuç beklenir.
+                    var result = producer.ProduceAsync("healthcare-topic", new Message<Null, string> { Value = message }).GetAwaiter().GetResult();
+                    
+                    //Mesajın teslim edildiği bilgisi ve konumu konsola yazdırılır.
+                    Console.WriteLine($"'{result.Value}' mesajı '{result.TopicPartitionOffset}' konumuna teslim edildi.");
+                }
+            }
+        }
+    }
+}
